022a0845a11b32f84284bd425a5f18f9a8ddc5ed
Shahreyar Arif <70688937+Shahreyar00@users.noreply.github.com>
2021-12-05 18:39:15 +0530

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,24 @@
+import "./home.css";
+import Chart from "../../components/chart/Chart";
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import { userData } from "../../dummyData";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
+
+
+const Home = () => {
+    return (
+        <div className="home">
+            <FeaturedInfo />
+            <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+            <div className="homeWidgets">
+                <WidgetSm/>
+                <WidgetLg/>
+            </div>
+        </div>
+    )
+}
+
+export default Home
+
+
